a972efa1635a8d927b0c278bf3965163ea544110
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-05-31 07:43:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -486,3 +486,9 @@ console.log(anyDeposits);
 // EVERY
 console.log(movements.every(mov => mov > 0));
 console.log(account4.movements.every(mov => mov > 0));
+
+// Separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
